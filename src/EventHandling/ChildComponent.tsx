f304920b77c508caf84a927108f30216291d0ab9
Rebecca Risha <rebecca.risha@rubicotech.in>
2022-03-07 17:07:00 +0530

    parameterized component in react js

--- src/EventHandling/ChildComponent.tsx
+++ src/EventHandling/ChildComponent.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 export interface Event {
     handleClick(data: React.MouseEvent<HTMLButtonElement>): void,
     name: string,
-    rollnumber?: number // means this is optional
+    rollnumber?: number // means this is optional,
+    handleParameter(value: string): void
 }
 
 export default function ChildComponent(props: Event) {
+    const [name, setName] = useState('');
     return (
         <div>
             {props.name}
-            <button onClick={props.handleClick}>CLick me</button>
+            <button onClick={props.handleClick}>Received event (e)</button><br />
+            <input type='text' onChange={(e) => setName(e.target.value)} />
+            <button onClick={() => props.handleParameter(name)}>Parameter passed</button>
         </div>
     )
 }
--- src/EventHandling/FunctionalHandling.tsx
+++ src/EventHandling/FunctionalHandling.tsx
@@ -4,11 +4,13 @@ import ChildComponent from './ChildComponent'
 export default function FunctionalHandling() {
     const handleClick = (e) => {
         console.log(e);
-    
+    }
+    const handleParameter = (data) => {
+        console.log(`displayed name is ${data}`);
     }
     return (
         <div>
-            <ChildComponent handleClick={(e)=>handleClick(e)} name='risha'  />
+            <ChildComponent handleClick={(e) => handleClick(e)} handleParameter={(data) => handleParameter(data)} name='risha' />
         </div>
     )
 }
